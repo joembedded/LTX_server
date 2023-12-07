f77b613cd9de33ea2b1433c5c8d2f5af3826f503
Jo <joembedded@gmail.com>
2023-12-07 20:07:30 +0100

    V2.225 weiter

--- sw/w_php/w_rad.php
+++ sw/w_php/w_rad.php
@@ -1,4 +1,5 @@
 <?php
+
 /***********************************************************
  * w_rad.php - push-cmd-pull worker fuer RemoteADmin 
  *
@@ -30,6 +31,15 @@ http://localhost/ltx/sw/w_php/w_rad.php?k=ABC&cmd=quotaget&s=DDC2FB99207A7E7E
 // Daten in 'quota' zu diesem Device aendern (nur was geaendert werden muss schicken)
 http://localhost/ltx/sw/w_php/w_rad.php?s=26FEA299F444F836&k=ABC&cmd=quotachange&quota[2]=neuerserver%20XXX (%20: SPACE)
 
+// Datei sys_param.lxp zu diesem Device holen (mit Beschreibung der Zeilen. Falls noch eine zu senden Datei aktiv: sysparam_pending:true)
+http://localhost/ltx/sw/w_php/w_rad.php?k=ABC&cmd=sysparamget&s=DDC2FB99207A7E7E
+
+// Daten in 'sys_param.lxp' zu diesem Device aendern (nur was geaendert werden mss schicken, hier Port 80 und Protokoll HTTP)
+http://localhost/ltx/sw/w_php/w_rad.php?k=ABC&cmd=sysparamchange&s=DDC2FB99207A7E7E&sys_param[10]=80&sys_param[19]=0
+
+// Pending 'sys_param.lxp' loeschen (falls vorhanden)
+http://localhost/ltx/sw/w_php/w_rad.php?k=ABC&cmd=sysparamunpend&s=DDC2FB99207A7E7E
+
 
  * Parameter - Koennen per POST oder URL uebergeben werden
  * cmd: Kommando
@@ -42,6 +52,9 @@ http://localhost/ltx/sw/w_php/w_rad.php?s=26FEA299F444F836&k=ABC&cmd=quotachange
  * details: Details zu einer MAC (quasi geich wie w_pcp.php)
  * quotaget: quota zu einer MAC holen
  * quotachange: quota (einzelne Eintraege oder alles) zu einer MAC aendern
+ * sysparamget: sys_param.lxp holen
+ * sysparamchange: sys_param.lxp aendern, ggf. neue Zeilen ergaenzen
+ * sysparamunpend: ggfs. pending sys_param.lxp loeschen
  * 
  * Status-Returns:
  * 0:	OK
@@ -50,10 +63,14 @@ http://localhost/ltx/sw/w_php/w_rad.php?s=26FEA299F444F836&k=ABC&cmd=quotachange
  * 102: Unbekanntes Kommando cmd
  * 103: Write Quota
  * 104: Index Error bei quota
+ * 105: sys_param not found
+ * 106: Index Error bei sys_param.lxp
+ * 107: No changes in sys_param.lxp
+ * 108: Write Error sys_param.lxp
  * ...
  */
 
-define('VERSION', "RAD V0.10 06.12.2023");
+define('VERSION', "RAD V0.11 07.12.2023");
 
 error_reporting(E_ALL);
 ini_set("display_errors", true);
@@ -73,6 +90,30 @@ $dbg = 0; // 1:Dbg, 2:Dbg++
 $fpath = "../" . S_DATA;	// Globaler Pfad auf Daten
 $xlog = ""; // Log-String
 
+// Beschreibung der Parameter damit leichter lesbar
+$p200_beschr = array(	// sys_param.lxp
+	"*@200_Sys_Param",
+	"APN[$41]",
+	"Server/VPN[$41]",
+	"Script/Id[$41]",
+	"API Key[$41]",
+	"ConFlags[0..255] (B0:Verbose B1:RoamAllow B4:LOG_FILE (B5:LOG_UART) B7:Debug)",
+	"SIM Pin[0..65535] (opt)",
+	"APN User[$41]",
+	"APN Password[$41]",
+	"Max_creg[10..255]",
+	"Port[1..65535]",
+	"Server_timeout_0[1000..65535]",
+	"Server_timeout_run[1000..65535]",
+	"Modem Check Reload[60..3600]",
+	"Bat. Capacity (mAh)[0..100000]",
+	"Bat. Volts 0%[float]",
+	"Bat. Volts 100%[float]",
+	"Max Ringsize (Bytes)[1000..2e31]",
+	"mAmsec/Measure[0..1e9]",
+	"Mobile Protocol[0..255] B0:0/1:HTTP/HTTPS B1:PRESET B2,3:TCP/UDPSetup"
+);
+
 // ------ Write LogFile (carefully and out-of-try()/catch()) -------- (similar to lxu_xxx.php)
 function add_logfile()
 {
@@ -86,11 +127,11 @@ function add_logfile()
 	if (!isset($mac)) $mac = "UNKNOWN_MAC";
 	if ($dbg) $xlog .= "(DBG:$dbg)";
 
-	$dstr=gmdate("d.m.y H:i:s ", $now) . "UTC ";
+	$dstr = gmdate("d.m.y H:i:s ", $now) . "UTC ";
 	$log = @fopen("$fpath/log/pcplog.txt", 'a');
 	if ($log) {
 		while (!flock($log, LOCK_EX)) usleep(10000);  // Lock File - Is a MUST
-		fputs($log,  $dstr. $_SERVER['REMOTE_ADDR']. " RAD");        // Write file
+		fputs($log,  $dstr . $_SERVER['REMOTE_ADDR'] . " RAD");        // Write file
 		if (strlen($mac)) fputs($log, " MAC:$mac"); // mac only for global lock
 		fputs($log, " $xlog\n");        // evt. add extras
 		flock($log, LOCK_UN);
@@ -106,7 +147,7 @@ function add_logfile()
 		$log = fopen("$fpath/$mac/pcplog.txt", 'a');
 		if (!$log) return;
 		while (!flock($log, LOCK_EX)) usleep(10000);  // Lock File - Is a MUST
-		fputs($log, $dstr."RAD $xlog\n");        // evt. add extras
+		fputs($log, $dstr . "RAD $xlog\n");        // evt. add extras
 		flock($log, LOCK_UN);
 		fclose($log);
 	}
@@ -117,7 +158,7 @@ try {
 	function checkAccess($lmac, $ckey)
 	{
 		global $fpath;
-		if($ckey == S_API_KEY) return true;	// S_API_KEY valid for ALL
+		if ($ckey == S_API_KEY) return true;	// S_API_KEY valid for ALL
 		$quota = @file("$fpath/$lmac/quota_days.dat", FILE_IGNORE_NEW_LINES);
 		if (isset($quota[2]) && strlen($quota[2])) {
 			$qpar = explode(' ', trim(preg_replace('/\s+/', ' ', $quota[2])));
@@ -132,28 +173,76 @@ try {
 	}
 
 	// Prueft Zahlenwert auf Grenzen - HELPER
-	function nverify($str, $ilow, $ihigh){
-		if(!is_numeric($str)) return true;
+	function nverify($str, $ilow, $ihigh)
+	{
+		if (!is_numeric($str)) return true;
 		$val = intval($str);
-		if($val<$ilow || $val>$ihigh) return true;	// Fehler
+		if ($val < $ilow || $val > $ihigh) return true;	// Fehler
 		return false;
 	}
-	function nisfloat($str){	// PHP recht relaxed, alles als Float OK, daher mind. 1 char. - HELPER
-		if(!is_numeric($str)) return true;
+	function nisfloat($str)
+	{	// PHP recht relaxed, alles als Float OK, daher mind. 1 char. - HELPER
+		if (!is_numeric($str)) return true;
 		return false;
 	}
 
 	// Pruefen ob quota OK (max. 3 Zeilen)
-	function checkquota($quota){
-		if(!isset($quota) || $quota == false) return "400 'quota_days.dat' not found";
-		if(count($quota)>3) return "401 'quota_days.dat' overdue lines";
+	function checkquota($quota)
+	{
+		if (!isset($quota) || $quota == false) return "400 'quota_days.dat' not found";
+		if (count($quota) > 3) return "401 'quota_days.dat' overdue lines";
 		$qd = intval(@$quota[0]);
-		if(!$qd || $qd>365000) return "402 'quota_days.dat' Days";
+		if (!$qd || $qd > 365000) return "402 'quota_days.dat' Days";
 		$ql = intval(@$quota[1]);
-		if($ql<100 ) return "403 'quota_days.dat' Lines<100";
+		if ($ql < 100) return "403 'quota_days.dat' Lines<100";
 		// Serverzeile ohne Pruefung
 		return null;	// OK
 	}
+
+	// Pruefen einer Sysparamdatei - return NULL (OK) oder Status - (wie in BlueShell: BlueBlx.cs)
+	function checksys_param($par)
+	{
+		// 1. Teil Pruefen der Gemeinsamen Parameter
+		if (!isset($par) || $par == false || count($par)<19 ) return "200 'sys_param.lxp' not found";
+		if ($par[0] !== '@200') return "201 File Format (No valid 'sys_param.lxp', ID must be '@200')";
+		if (strlen($par[1]) > 41) return "202 APN Len";
+		if (strlen($par[2]) > 41) return "203 Server Len";
+		if (strlen($par[3]) > 41) return "204 Script Len";
+		if (strlen($par[4]) > 41) return "205 API Key Len";
+		if (nverify($par[5], 0, 255)) return "206 ConFlags";
+		if (nverify($par[6], 0, 65535)) return "207 SIM PIN";
+		if (strlen($par[7]) > 41) return "208 User Len";
+		if (strlen($par[8]) > 41) return "209 Password Len";
+		if (nverify($par[9], 10, 255)) return "210 Max_creg";
+		if (nverify($par[10], 1, 65535)) return "211 Port";
+		if (nverify($par[11], 1000, 65535)) return "212 Timeout_0";
+		if (nverify($par[12], 1000, 65535)) return "213 Timeout_run";
+		if (nverify($par[13], 60, 3600)) return "214 Reload";
+		if (nverify($par[14], 0, 100000)) return "215 Bat. Capacity";
+		if (nisfloat($par[15])) return "216 Bat. Volt 0%";
+		if (nisfloat($par[16])) return "217 Bat. Volt 100%";
+		if (nverify($par[17], 1000, 0x7FFFFFFF)) return "218 Max Ringsize";
+		if (nverify($par[18], 0, 1000000000)) return "219 mAmsec/Measure";
+		if (count($par) >19) if (nverify($par[19], 0, 255)) return "220 Mobile Protocol";
+		return null;	// OK
+	}
+
+	function getcurrentsys_param()
+	{
+		global $fpath, $mac, $retResult, $status;
+		$par = @file("$fpath/$mac/put/sys_param.lxp", FILE_IGNORE_NEW_LINES); // pending Parameters?
+		if ($par != false) {
+			$retResult['syspar_pending'] = true;	// Return - Pending Parameters!
+		} else {
+			$par = @file("$fpath/$mac/files/sys_param.lxp", FILE_IGNORE_NEW_LINES); // No NL, but empty Lines OK
+			if ($par == false) {
+				$status = "105 No 'sys_param.lxp' found for MAC:$mac";
+			}
+			$retResult['syspar_pending'] = false;	// On Dev.
+		}
+		return $par;
+	}
+
 
 	//=========== MAIN ==========
 	$retResult = array();
@@ -163,9 +252,9 @@ try {
 	$cmd = @$_REQUEST['cmd'];
 	if (!isset($cmd)) $cmd = "";
 	$ckey = @$_REQUEST['k'];	// s always MAC (k: API-Key, r: Reason)
-	if($ckey == S_API_KEY) $xlog = "(cmd:'$cmd')"; // internal
+	if ($ckey == S_API_KEY) $xlog = "(cmd:'$cmd')"; // internal
 	else $xlog = "(cmd:'$cmd', k:'$ckey')";
-	
+
 
 	// --- cmd PreStart - CMD vorfiltern ---
 	switch ($cmd) {
@@ -213,7 +302,7 @@ try {
 			$devres = $statement->fetch(); // $devres[]: 'device(mac)'!
 
 			$ovv = array();	// Overview zu dieser MAC
-			$ovv['mac']=$mac;   
+			$ovv['mac'] = $mac;
 			$ovv['db_now'] = $pdo->query("SELECT NOW() as now")->fetch()['now']; // *JETZT* als Datum UTC - Rein zurInfo
 
 			$retResult['overview'] = $ovv;
@@ -223,7 +312,7 @@ try {
 	switch ($cmd) {
 		case '': // VERSION
 		case 'list': // Liste schon fertig
-			break;	
+			break;
 
 		case 'details':	// Einfach ALLES fuer diese MAC
 			$retResult['details'] = $devres;
@@ -233,7 +322,7 @@ try {
 			// Original holen und pruefen
 			$quota = @file("$fpath/$mac/quota_days.dat", FILE_IGNORE_NEW_LINES);
 			$chkres = checkquota($quota);
-			if($chkres != null) { 
+			if ($chkres != null) {
 				$status = $chkres;
 				break;
 			}
@@ -247,7 +336,7 @@ try {
 			$nqlist = $_REQUEST['quota'];
 			foreach ($nqlist as $npk => $npv) {
 				$idx = intval($npk);
-				if ($idx < 0 || $idx > 2) { 
+				if ($idx < 0 || $idx > 2) {
 					$status = "104 Index Error";
 					break;
 				}
@@ -260,7 +349,7 @@ try {
 			}
 			// geaenderte Quota vor Schreiben pruefen
 			$chkres = checkquota($quota);
-			if($chkres != null) { 
+			if ($chkres != null) {
 				$status = $chkres;
 				break;
 			}
@@ -275,10 +364,97 @@ try {
 				$status = "103 Write 'quota_days.dat'";
 			}
 			break;
+
+		case 'sysparamget':
+			$par = getcurrentsys_param();
+			$chkres = checksys_param($par);
+			if($chkres != null) { 
+				$status = $chkres;
+				break;
+			}
+			$vkarr = [];	// Ausgabe der Parameter etwas verzieren fuer leichtere Lesbarkeit
+			$infoarr = $p200_beschr;
+			$lcnt=0;
+			foreach ($par as $p) {
+				$info = @$infoarr[$lcnt];
+				if(!isset($info)) $info = "(Undef.)"; // Unkown
+				$info .= " (Line $lcnt)"; // Juer jede Zeile: Erklaere Bedeutung
+				$vkarr[] = array('line' => $p, 'info' => $info); // Line, Value, Text
+				$lcnt++;
+			}
+			$ipov = array(); // Parameter Overview
+			$retResult['sys_param'] = $vkarr;
+			break;
+
+		case 'sysparamchange':
+			$opar = $par = getcurrentsys_param();
+			$chkres = checksys_param($par);
+			if($chkres != null) { 
+				$status = $chkres;
+				break;
+			}
+			$nparlist = $_REQUEST['sys_param'];
+			foreach ($nparlist as $npk => $npv) {
+				$idx = intval($npk);
+				if ($idx < 1 || $idx > 30) { // Mehr als 30 Zeilen erstmal nie
+					$status = "106 Index Error";
+					break;
+				}
+				// Bei Bedarf neue Linien erzeugen, solange neuer Idx ausserhalb von existierendem:
+				while ($idx > count($par)) { 
+					$par[] = "";
+				}
+			}
+			if (isset($status)) break;
+			// Nun alles OK, Alte Werte durch neue ersetzen
+			foreach ($nparlist as $npk => $npv) {
+				$idx = intval($npk);
+				$par[$idx] = $npv;
+			}
+			$chkres = checksys_param($par); // Nochmal pruefen
+			if($chkres != null) { 
+				$status = $chkres;
+				break;
+			}
+			// Auf Delta pruefen 
+			if(count($opar)==count($par)){
+				for($i=0;$i<count($opar);$i++){
+					if($opar[$i]!=$par[$i]) break;
+				}
+				if($i==count($opar)){
+					$status = "107 No Changes found"; // Keine Aederungen
+					break;
+				}
+			}
+			// Aus Array File erzeugen
+			$nparstr = implode("\n", $par) . "\n";
+			$ilen = strlen($nparstr);
+			@unlink("$fpath/$mac/cmd/sys_param.lxp.pmeta"); 
+			if ($ilen > 32)	$slen = file_put_contents("$fpath/$mac/put/sys_param.lxp", $nparstr); // unter 32 len nicht sinnvoll
+			else $slen = -1;
+			if ($ilen == $slen) {
+				file_put_contents("$fpath/$mac/cmd/sys_param.lxp.pmeta", "sent\t0\n");
+				$wnpar = @file("$fpath/$mac/put/sys_param.lxp", FILE_IGNORE_NEW_LINES); // Set NewName?
+				$xlog .= "(New Hardware-Parameter 'sys_param.lxp':$ilen)";
+				$retResult['syspar_pending'] = true;
+			} else {
+				$xlog .= "(ERROR: Write 'sys_param.lxp':$slen/$ilen Bytes)";
+				$status = "108 Write Parameter";
+			}
+			break;
+
+			case 'sysparamunpend': // Remove pending sys_param.lxp
+				@unlink("$fpath/$mac/cmd/sys_param.lxp.pmeta");
+				@unlink("$fpath/$mac/put/sys_param.lxp");
+				$par = getcurrentsys_param();
+				if ($par == false) break;
+				$xlog .= "(Remove pending Hardware-Parameter'sys_param.lxp')";
+				break;
 	
-/****************************************************************
-* AB hier weitere eigene CMDS *todo* 
-****************************************************************/
+
+			/****************************************************************
+			 * AB hier weitere eigene CMDS *todo* 
+			 ****************************************************************/
 
 		default:
 			$status = "102 Unknown Cmd";
